99f7aefb9fa2123923892da7aa0c4e147560a847
silvioivanio <silvioivanio29@gmail.com>
2025-11-17 20:27:37 +0200

    Add functionality on the buttons

---
+++ index.js
@@ -0,0 +1,54 @@
+// HOME BUTTONS GETTERS
+let onePointHomeBtn = document.querySelector("#one-point-home-btn");
+let twoPointsHomeBtn = document.querySelector("#two-points-home-btn");
+let treePointsHomeBtn = document.querySelector("#tree-points-home-btn");
+
+
+// GUEST BUTTONS GETTERS
+let onePointGuestBtn = document.querySelector("#one-point-guest-btn");
+let twoPointsGuestBtn = document.querySelector("#two-points-guest-btn");
+let treePointsGuestBtn = document.querySelector("#tree-points-guest-btn");
+
+
+// GET HOME SCORE
+let scoreHome = document.querySelector("#score-home");
+let scoreHomeResult = parseInt(scoreHome.textContent); // Covert to integer
+
+
+// GET GUEST SCORE
+let scoreGuest = document.querySelector("#score-guest")
+let scoreGuestResult = parseInt(scoreGuest.textContent); // Covert to integer
+
+
+// HOME SCORE TRIGGERS
+onePointHomeBtn.addEventListener("click", function() {
+    scoreHomeResult += 1;
+    scoreHome.textContent = scoreHomeResult;
+});
+
+twoPointsHomeBtn.addEventListener("click", function() {
+    scoreHomeResult += 2;
+    scoreHome.textContent = scoreHomeResult;
+});
+
+treePointsHomeBtn.addEventListener("click", function() {
+    scoreHomeResult += 3;
+    scoreHome.textContent = scoreHomeResult;
+});
+
+
+// GUEST SCORE TRIGGERS
+onePointGuestBtn.addEventListener("click", function() {
+    scoreGuestResult += 1;
+    scoreGuest.textContent = scoreGuestResult;
+});
+
+twoPointsGuestBtn.addEventListener("click", function() {
+    scoreGuestResult += 2;
+    scoreGuest.textContent = scoreGuestResult;
+});
+
+treePointsGuestBtn.addEventListener("click", function() {
+    scoreGuestResult += 3;
+    scoreGuest.textContent = scoreGuestResult;
+});
